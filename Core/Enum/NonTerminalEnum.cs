0280998c96898083f99e92887ed9008eaa087f47
Gabriel <gabrielfernandesfg@hotmail.com>
2019-09-25 00:31:44 -0300

    Initial ideas for parsingtable

--- Core/Enum/NonTerminalEnum.cs
+++ Core/Enum/NonTerminalEnum.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Core.Enum
 {
-    public enum NoTerminalEnum
+    public enum NonTerminalEnum
     {
 
         PROGRAMA = 52,
@@ -17,6 +17,32 @@ namespace Core.Enum
         DCLVAR = 59,
         LDVAR = 60,
         TIPO = 61,
-        DCLPROC = 62
+        DCLPROC = 62,
+        DEFPAR = 63,
+        CORPO = 64,
+        REPCOMANDO = 65,
+        COMANDO = 66,
+        RCOMID = 67,
+        RVAR = 68,
+        PARAMETROS = 69,
+        REPPAR = 70,
+        ELSEPARTE = 71,
+        VARIAVEL = 72,
+        VARIAVEL1 = 73,
+        REPVARIAVEL = 74,
+        ITEMSAIDA = 75,
+        REPITEM = 76,
+        EXPRESSAO = 77,
+        REPEXPSIMP = 78,
+        EXPSIMP = 79,
+        REPEXP = 80,
+        TERMO = 81,
+        REPTERMO = 82,
+        FATOR = 83,
+        CONDCASE = 84,
+        CONTCASE = 85,
+        RPINTEIRO = 86,
+        SEMEFEITO = 87
+
     }
 }
--- Core/ParsingTable.cs
+++ Core/ParsingTable.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class ParsingTable
+    {
+        public IDictionary<(int, int), string> ParsingTable = new Dictionary<(int, int), string>
+        {
+
+        };
+
+        }
+
+}
