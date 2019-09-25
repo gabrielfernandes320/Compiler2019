871ad457e503ced4a391bc7c5c37f6d84cab3b6b
Edvaldo da Rosa <fraterblack@bck.com.br>
2019-09-24 21:00:03 -0300

    Fixing typo

--- Core/Enum/NonTerminalEnum.cs
+++ Core/Enum/NonTerminalEnum.cs
@@ -7,16 +7,16 @@ namespace Core.Enum
     public enum NoTerminalEnum
     {
 
-        PROGRAMA = 52
-        BLOCO = 53
-        DCLROT = 54
-        LID = 55
-        REPIDENT = 56
-        DCLCONST = 57
-        LDCONST = 58
-        DCLVAR = 59
-        LDVAR = 60
-        TIPO = 61
+        PROGRAMA = 52,
+        BLOCO = 53,
+        DCLROT = 54,
+        LID = 55,
+        REPIDENT = 56,
+        DCLCONST = 57,
+        LDCONST = 58,
+        DCLVAR = 59,
+        LDVAR = 60,
+        TIPO = 61,
         DCLPROC = 62
     }
 }
