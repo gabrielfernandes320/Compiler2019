da030f6a012debfd60dd53f977a6226b4669fc6d
Gabriel <gabrielfernandesfg@hotmail.com>
2019-11-02 23:45:00 -0300

    Start of semantic analysis

---
+++ Core/SemanticAnalysis/Identifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.SemanticAnalysis
+{
+    public class Identifier
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public int Level { get; set; }
+
+    }
+}
---
+++ Core/SemanticAnalysis/SemanticAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.SemanticAnalysis
+{
+    public class SemanticAnalyzer
+    {
+        List<Identifier> identifiersList = new List<Identifier>();
+
+        public bool Insert(Token token)
+        {
+            return true;
+        }
+    }
+}
